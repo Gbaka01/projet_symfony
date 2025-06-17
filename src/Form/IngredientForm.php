b8f196fc783ba6c1f1b126405584a0ed73e20754
gbaka01 <gore.goli@gmail.com>
2025-06-17 15:58:44 +0200

    commitgg7

--- src/Form/IngredientForm.php
+++ src/Form/IngredientForm.php
@@ -2,10 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Categorie;
 use App\Entity\Ingredient;
 use App\Form\IngredientForm;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
@@ -23,6 +25,13 @@ class IngredientForm extends AbstractType
                     'class' => 'form-control'
                 ]
                 ])
+            ->add ('categorie', EntityType::class, [
+                'multiple' => true,
+                'class' => Categorie::class,
+                'choice_label' => 'description2',
+                'expanded' => true,
+            
+            ])    
         ;
     }
 
